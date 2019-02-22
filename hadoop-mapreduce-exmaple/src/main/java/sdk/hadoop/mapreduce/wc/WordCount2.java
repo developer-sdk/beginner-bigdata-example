2f758d13f031996e7778e259f272d74f55178033
118k <fluorite118@gmail.com>
2019-02-22 21:04:10 +0900

    주석 추가

--- hadoop-mapreduce-exmaple/src/main/java/sdk/hadoop/mapreduce/wc/WordCount2.java
+++ hadoop-mapreduce-exmaple/src/main/java/sdk/hadoop/mapreduce/wc/WordCount2.java
@@ -38,6 +38,7 @@ public class WordCount2 {
 
 	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
+		// 문자의 개수를 세는 카운터 
 		static enum CountersEnum {
 			INPUT_WORDS
 		}
@@ -45,17 +46,17 @@ public class WordCount2 {
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
 
-		private boolean caseSensitive;
-		private Set<String> patternsToSkip = new HashSet<String>();
+		private boolean caseSensitive;	// 대소문자 구분 
+		private Set<String> patternsToSkip = new HashSet<String>();	// 문자에서 제거할 기호 
 
 		private Configuration conf;
-		private BufferedReader fis;
 
+		// 맵작업의 설정 
 		@Override
 		public void setup(Context context) throws IOException, InterruptedException {
 			conf = context.getConfiguration();
-			caseSensitive = conf.getBoolean("wordcount.case.sensitive", false);
-			if (conf.getBoolean("wordcount.skip.patterns", false)) {
+			caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);	// 대소문자 구분 
+			if (conf.getBoolean("wordcount.skip.patterns", false)) {	// 스킵 패턴 파일을 분산 캐쉬에서 가져와서 patternsToSkip에 설정 
 				URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
 				for (URI patternsURI : patternsURIs) {
 					Path patternsPath = new Path(patternsURI.getPath());
@@ -66,29 +67,33 @@ public class WordCount2 {
 		}
 
 		private void parseSkipFile(String fileName) {
-			try {
-				fis = new BufferedReader(new FileReader(fileName));
+			try (BufferedReader fis = new BufferedReader(new FileReader(fileName))) {
 				String pattern = null;
 				while ((pattern = fis.readLine()) != null) {
 					patternsToSkip.add(pattern);
 				}
 			} catch (IOException ioe) {
-				System.err.println(
-						"Caught exception while parsing the cached file '" + StringUtils.stringifyException(ioe));
+				System.err.println("Caught exception while parsing the cached file '" + StringUtils.stringifyException(ioe));
 			}
 		}
 
 		@Override
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
 			String line = (caseSensitive) ? value.toString() : value.toString().toLowerCase();
+			
+			// patternsToSkip과 일치하는 문자를 제거 
 			for (String pattern : patternsToSkip) {
 				line = line.replaceAll(pattern, "");
 			}
+			
 			StringTokenizer itr = new StringTokenizer(line);
 			while (itr.hasMoreTokens()) {
 				word.set(itr.nextToken());
 				context.write(word, one);
-				Counter counter = context.getCounter(CountersEnum.class.getName(), CountersEnum.INPUT_WORDS.toString());
+				
+				// 카운터에 1을 더함  
+				Counter counter = context.getCounter("User Custom Counter", CountersEnum.INPUT_WORDS.toString());
 				counter.increment(1);
 			}
 		}
@@ -97,8 +102,7 @@ public class WordCount2 {
 	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 		private IntWritable result = new IntWritable();
 
-		public void reduce(Text key, Iterable<IntWritable> values, Context context)
-				throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 			int sum = 0;
 			for (IntWritable val : values) {
 				sum += val.get();
@@ -110,14 +114,18 @@ public class WordCount2 {
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-
+		
+		// 옵션 파서를 이용해서 옵션의 개수가 2 또는 4개가 아니면 오류 메시지 출력후 종료
+		// -D로 전달되는 옵션은 conf에 설정 
 		GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
 		String[] remainingArgs = optionParser.getRemainingArgs();
 		if (!(remainingArgs.length != 2 || remainingArgs.length != 4)) {
 			System.err.println("Usage: wordcount <in> <out> [-skip skipPatternFile]");
 			System.exit(2);
 		}
-		Job job = Job.getInstance(conf, "word count");
+		
+		// 맵리듀스 잡 설정 
+		Job job = Job.getInstance(conf, "word count 2");
 		job.setJarByClass(WordCount2.class);
 		job.setMapperClass(TokenizerMapper.class);
 		job.setCombinerClass(IntSumReducer.class);
@@ -125,13 +133,15 @@ public class WordCount2 {
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 
+		// 옵션으로 -skip 으로 전달된 파일 경로를 분산 캐쉬에 추가하고,
+		// wordcount.skip.patterns를 true로 설정 
 		List<String> otherArgs = new ArrayList<String>();
 		for (int i = 0; i < remainingArgs.length; ++i) {
 			if ("-skip".equals(remainingArgs[i])) {
 				job.addCacheFile(new Path(remainingArgs[++i]).toUri());
-				job.getConfiguration().setBoolean("wordcount.skip.patterns", false);
+				job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
 			} else {
-				otherArgs.add(remainingArgs[i]);
+				otherArgs.add(remainingArgs[i]);	// 파일경로 
 			}
 		}
 		FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
